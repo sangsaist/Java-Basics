2103cb625cc06b51d054d0fb7f1b901561a97f7d
sangsaist <sanjaysangsai@gmail.com>
2025-03-06 00:12:59 +0530

    adding the add.java file

---
+++ var_datatype/add.java
@@ -0,0 +1,11 @@
+package var_datatype;
+
+public class add {
+    public static void main(String[] args) {
+    int a=20;
+    int b=30;
+    int sum= a+b;
+    System.out.println("Sum: "+ sum); 
+}
+
+}
