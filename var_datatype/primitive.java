9dca1615d14b66a2823358b12884d687e464f676
sangsaist <sanjaysangsai@gmail.com>
2025-03-06 00:06:02 +0530

    adding the primitive.java file

---
+++ var_datatype/primitive.java
@@ -0,0 +1,15 @@
+package var_datatype;
+//Program to Store and Print Different Data Types
+public class primitive {
+    static int age = 67;
+    static double height = 6.7;
+    static char section = 'B';
+    static boolean areyoufine = true;
+
+    public static void main(String[] args) {
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Section: " + section);
+        System.out.println("Is Java Fun? " + areyoufine);
+    }
+}
