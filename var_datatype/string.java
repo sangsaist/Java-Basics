9e63fb262a499a25fc34c9db29d46425ecc763f9
sangsaist <sanjaysangsai@gmail.com>
2025-03-06 00:18:58 +0530

    string.java

---
+++ var_datatype/string.java
@@ -0,0 +1,11 @@
+package var_datatype;
+//example for string 
+public class string {
+    public static void main(String[] args) {
+        String peru = "sangsai";
+        String initial="ST";
+        System.out.println("My name: " + peru + " " + initial);
+
+    }
+    
+}
