f124724fbdc85318509c6ad59d5eb1620814f04b
sangsaist <sanjaysangsai@gmail.com>
2025-03-06 22:26:25 +0530

    Bitwise

---
+++ operators/bitwise.java
@@ -0,0 +1,14 @@
+package operators;
+
+public class bitwise {
+    public static void main(String[] args) {
+        int a = 5, b = 3;
+        System.out.println(a & b);  
+        System.out.println(a | b);  
+        System.out.println(a ^ b);  
+        System.out.println(~a);  
+        System.out.println(a << 1);  
+        System.out.println(a >> 1);  
+    }
+    
+}
---
+++ operators/bitwise2.java
@@ -0,0 +1,22 @@
+package operators;
+import java.util.Scanner;
+public class bitwise2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+
+        sc.close();
+    }
+    
+}
