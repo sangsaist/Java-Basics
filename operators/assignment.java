a0c728372b3221e41878f7b93dd0d922ed8fcc12
sangsaist <sanjaysangsai@gmail.com>
2025-03-06 22:10:00 +0530

    Assignment

---
+++ operators/assignment.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class assignment{
+    public static void main(String[] args) {
+        int a = 20; int b = 30;
+        a += 20;
+        b -=30;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
---
+++ operators/assignment2.java
@@ -0,0 +1,17 @@
+package operators;
+import java.util.Scanner;
+public class assignment2 {
+    public static void main(String[] args) {
+        Scanner as2 = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int a = as2.nextInt();
+        System.out.println("Enter the second number: ");
+        int b = as2.nextInt();
+        a *= 20;
+        b %= 40;
+        System.out.println("The value of first number after multiplication: "+ a);
+        System.out.println("The value of second number after modulus: "+ a);
+        as2.close();
+
+    }
+}
