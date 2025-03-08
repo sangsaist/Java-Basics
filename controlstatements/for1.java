cc1417e3345928c5922dc279bf75763a8b5d5bc1
sangsaist <sanjaysangsai@gmail.com>
2025-03-08 14:27:05 +0530

    postincrement

---
+++ controlstatements/for1.java
@@ -0,0 +1,10 @@
+package controlstatements;
+
+public class for1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
